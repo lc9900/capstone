851beda19eb6812590b4a3ae08b4fa4f228dfeba
lc9900 <whynot_hm@hotmail.com>
2017-11-10 12:40:51 -0500

    seeded places, user, user_friends_map, user_place_map

--- server/db/seed.js
+++ server/db/seed.js
@@ -1,21 +1,51 @@
-const db = require('./server/db');
+const db = require('./');
 const { Sequelize } = db;
 
 //Models
-const {User} = require('./server/db/models');
-
+const {User, Place} = require('./models');
+let seedUsers = [],
+    seedPlaces = [];
 
 db.sync({force:true})
     .then(() => {
         seed();
     })
+
 const seed = () => {
     return Promise.all([
-    User.create({ name: 'Rav', email: 'ravsworld@gmail.com', password: 'password' }),
-    User.create({ name: 'Annie', email: 'annielovescode@gmail.com', password: 'password' }),
-    User.create({ name: 'admin', email: 'admin@gmail.com', password: 'admin', admin: true })
+    User.create({ name: 'Bob', email: 'Bob@capstone.com', password: '123' }),
+    User.create({ name: 'Boyoon', email: 'boyoon@capstone.com', password: '123' }),
+    User.create({ name: 'AJ', email: 'aj@capstone.com', password: '123' }),
+    User.create({ name: 'Han', email: 'han@capstone.com', password: '123' }),
   ])
+    .then((users) => {
+        seedUsers = users;
+        return Promise.all([
+            users[0].setFriends([users[1], users[2], users[3]]),
+            users[1].setFriends([users[0], users[2], users[3]]),
+            users[2].setFriends([users[1], users[0], users[3]]),
+            users[3].setFriends([users[1], users[2], users[0]])
+        ])
+    })
+    .then(() => {
+        return Promise.all([
+            Place.create({name: 'Home', address: '220 W 121st St, New York, NY 10027'}),
+            Place.create({name: 'Home', address: '20 W 102nd St, New York, NY 10025'}),
+            Place.create({name: 'Home', address: '1411 Broadway, New York, NY 10018'}),
+            Place.create({name: 'Home', address: '75 Maiden Ln, New York, NY 10038'}),
+        ]);
+    })
+    .then(places => {
+        seedPlaces = places
+        return Promise.all([
+            seedUsers[0].addPlace(places[0]),
+            seedUsers[1].addPlace(places[1]),
+            seedUsers[2].addPlace(places[2]),
+            seedUsers[3].addPlace(places[3])
+        ]);
+    })
     .then(() => console.log('seeded!'))
+    .then(() => db.close())
     .catch(err => { throw err; });
 }
 
