149a798d60834dec33775182caa92b002973ab47
AJ Frank <alexanderjfrank@gmail.com>
2017-11-09 20:12:33 -0500

    nav initial commit

---
+++ client/components/Nav.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function Nav(props) {
+  return (
+    <div>
+      <ul className="nav nav-pills nav-fill">
+        <li className="nav-item">
+          <a className="nav-link" href="#">
+            Item one
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">
+            Item one
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+}
--- client/reducers/cart-sample.js
+++ client/reducers/cart-sample.js
@@ -1,20 +1,18 @@
-import axios from 'axios';
+import axios from "axios";
 
 //Action Types
 
-
-
 //Action Creators
-
 
 //Thunk Creators
 
 //Cart Reducer
 const cartReducer = function(state = {}, action) {
-	switch(action.type) {
-		// case GET_CART : return action.cart
-		default: return state
-	}
+  switch (action.type) {
+    // case GET_CART : return action.cart
+    default:
+      return state;
+  }
 };
 
-export default cartReducer
+export default cartReducer;
