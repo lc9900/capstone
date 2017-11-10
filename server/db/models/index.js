a345c0036044971b280499b55e2e68ce27c69ae7
lc9900 <whynot_hm@hotmail.com>
2017-11-09 23:42:15 -0500

    modifying some associations

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -3,19 +3,28 @@ const Sequelize = db.Sequelize;
 const User = require('./User');
 const Meetup = require('./Meetup');
 const Place = require('./Place');
+const MeetupUserStatus = require('./MeetupUserStatus');
 
 // Association
 User.belongsToMany(User, {as: 'friends', through: 'user_friends_map'});
+
+// For user's place
+User.belongsToMany(Place, {through: 'user_place_map'});
+Place.belongsToMany(User, {through: 'user_place_map'});
+
 Meetup.belongsTo(Place);
 Place.hasMany(Meetup);
 
 Meetup.belongsToMany(User, {through: 'user_meetup_map'});
 User.belongsToMany(Meetup, {through: 'user_meetup_map'});
 
+MeetupUserStatus.belongsTo(User);
+MeetupUserStatus.belongsTo(Meetup);
+User.hasMany(MeetupUserStatus);
+Meetup.hasMany(MeetupUserStatus);
 
 // Wrapper
 
-
 module.exports = {
-    User, Meetup, Place
+    User, Meetup, Place, MeetupUserStatus
 }
