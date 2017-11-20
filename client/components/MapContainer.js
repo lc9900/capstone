35f481c4d6656c2f3630fb151b4d2b5af07572c0
AJ Frank <alexanderjfrank@gmail.com>
2017-11-20 17:28:33 -0500

    a lot of mapping stuff

--- client/components/MapContainer.js
+++ client/components/MapContainer.js
@@ -1,212 +1,99 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react";
+import { GoogleApiWrapper, Map, Marker, InfoWindow } from "google-maps-react";
+import store from "../store";
 
 export class MapContainer extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+    this.state = props;
+    this.onMarkerClick = this.onMarkerClick.bind(this);
+  }
+
+  onMarkerClick(props, marker, e) {
+    console.log("props", props);
+    console.log("marker", marker);
+
+    this.setState({
+      showInfoWindow: true,
+      activeMarker: marker,
+      selectedPlace: props
+    });
   }
 
   render() {
-    if (!this.props.loaded) {
+    if (!this.props.loaded || !this.state) {
       return <div>Loading...</div>;
     }
+
+    console.log("current state", this.state);
+
+    const { onMarkerClick } = this;
+    const { google } = this.props;
+    const { showInfoWindow, activeMarker, selectedPlace, map } = this.state;
     const style = {
       width: "70%",
-      height: "70%"
+      height: "70%",
+      position: "relative"
     };
+
     return (
-      <div className="row">
+      <div>
+        <h1>MapContainer</h1>
         <Map
-          className="gmap"
-          google={this.props.google}
+          google={google}
           style={style}
-          initialCenter={{ lat: 40.705312, lng: -74.009197 }}
-          zoom={15}
-        />
+          initialCenter={{ lat: 40.752051, lng: -73.957088 }}
+          zoom={12}
+        >
+          <Marker
+            onClick={onMarkerClick}
+            title={"Your Starting Destination"}
+            name={"User Start"}
+            position={{ lat: map.userStart.lat, lng: map.userStart.lng }}
+          />
+          <Marker
+            onClick={onMarkerClick}
+            title={"Friend's Starting Destination"}
+            name={"Friend Start"}
+            position={{ lat: map.friendStart.lat, lng: map.friendStart.lng }}
+          />
+          <Marker
+            onClick={onMarkerClick}
+            title={"Meeting Destination"}
+            name={"Meeting Destination"}
+            position={{
+              lat: map.meetingDestination.lat,
+              lng: map.meetingDestination.lng
+            }}
+          />
+          <InfoWindow marker={activeMarker} visible={showInfoWindow}>
+            <div>
+              <h4>{selectedPlace.name}</h4>
+            </div>
+          </InfoWindow>
+        </Map>
       </div>
     );
   }
 }
 
-export default GoogleApiWrapper({
-  apiKey: "AIzaSyAopJDwUG1vlrsZg94qP6yuPtzapUgYw8g"
-})(MapContainer);
+const mapState = ({ user, map }) => {
+  return {
+    user,
+    map,
+    showInfoWindow: false,
+    activeMarker: {},
+    selectedPlace: {}
+  };
+};
 
-// let midpointLat = 40.771;
-// let midpointLng = -73.974;
-// var map;
+const mapDispatch = dispatch => {
+  return {};
+};
 
-// function initMap() {
-//   map = new google.maps.Map(document.getElementById("map"), {
-//     mapTypeControl: false,
-//     center: { lat: 40.771, lng: -73.974 },
-//     zoom: 13
-//   });
-//   new AutocompleteDirectionsHandler(map);
-// }
-
-// /**
-//   * @constructor
-//  */
-// function AutocompleteDirectionsHandler(map) {
-//   this.map = map;
-//   this.originPlaceId = null;
-//   this.destinationPlaceId = null;
-//   this.travelMode = "WALKING";
-//   var originInput = document.getElementById("origin-input");
-//   var destinationInput = document.getElementById("destination-input");
-//   var modeSelector = document.getElementById("mode-selector");
-//   this.directionsService = new google.maps.DirectionsService();
-//   this.directionsDisplay = new google.maps.DirectionsRenderer();
-//   this.directionsDisplay.setMap(map);
-
-//   var originAutocomplete = new google.maps.places.Autocomplete(originInput, {
-//     placeIdOnly: true
-//   });
-//   var destinationAutocomplete = new google.maps.places
-//     .Autocomplete(destinationInput, { placeIdOnly: true });
-
-//   this.setupClickListener("changemode-walking", "WALKING");
-//   this.setupClickListener("changemode-transit", "TRANSIT");
-//   this.setupClickListener("changemode-driving", "DRIVING");
-
-//   this.setupPlaceChangedListener(originAutocomplete, "ORIG");
-//   this.setupPlaceChangedListener(destinationAutocomplete, "DEST");
-
-//   this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(originInput);
-//   this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(
-//     destinationInput
-//   );
-//   this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(modeSelector);
-// }
-
-// // Sets a listener on a radio button to change the filter type on Places
-// // Autocomplete.
-// AutocompleteDirectionsHandler.prototype.setupClickListener = function(
-//   id,
-//   mode
-// ) {
-//   var radioButton = document.getElementById(id);
-//   var me = this;
-//   radioButton.addEventListener("click", function() {
-//     me.travelMode = mode;
-//     me.route();
-//   });
-// };
-
-// AutocompleteDirectionsHandler.prototype.setupPlaceChangedListener = function(
-//   autocomplete,
-//   mode
-// ) {
-//   var me = this;
-//   autocomplete.bindTo("bounds", this.map);
-//   autocomplete.addListener("place_changed", function() {
-//     var place = autocomplete.getPlace();
-//     if (!place.place_id) {
-//       window.alert("Please select an option from the dropdown list.");
-//       return;
-//     }
-//     if (mode === "ORIG") {
-//       me.originPlaceId = place.place_id;
-//     } else {
-//       me.destinationPlaceId = place.place_id;
-//     }
-//     me.route();
-//   });
-// };
-
-// AutocompleteDirectionsHandler.prototype.route = function() {
-//   if (!this.originPlaceId || !this.destinationPlaceId) {
-//     return;
-//   }
-//   var me = this;
-
-//   this.directionsService.route(
-//     {
-//       origin: { placeId: this.originPlaceId },
-//       destination: { placeId: this.destinationPlaceId },
-//       travelMode: this.travelMode
-//     },
-//     function(response, status) {
-//       const resp = response;
-//       const tripDuration = response.routes[0].legs[0].duration.value;
-//       console.log("response:", response);
-//       console.log("Duration full trip (secs):", tripDuration);
-//       const steps = response.routes[0].legs[0].steps;
-//       let prevAccumDuration = 0;
-//       let newAccumDuration = 0;
-//       let logged = false;
-//       for (var i = 0; i < steps.length; i++) {
-//         const stepDuration = steps[i].duration.value;
-//         newAccumDuration = prevAccumDuration + stepDuration;
-//         if (newAccumDuration > tripDuration / 2 && !logged) {
-//           console.log("** Halfway point on leg " + i);
-//           const stepStartLat = steps[i].start_location.lat();
-//           const stepStartLng = steps[i].start_location.lng();
-//           const stepEndLat = steps[i].end_location.lat();
-//           const stepEndLng = steps[i].end_location.lng();
-//           console.log("stepStart:", stepStartLat, stepStartLng);
-//           console.log("stepEnd:", stepEndLat, stepEndLng);
-//           const percentIntoStepForMidway =
-//             (tripDuration / 2 - prevAccumDuration) / stepDuration;
-//           midpointLat =
-//             stepStartLat +
-//             (stepEndLat - stepStartLat) * percentIntoStepForMidway;
-//           midpointLng =
-//             stepStartLng +
-//             (stepEndLng - stepStartLng) * percentIntoStepForMidway;
-//           console.log("midpoint:", midpointLat, midpointLng);
-//           logged = true;
-//         }
-//         prevAccumDuration = newAccumDuration;
-//         console.log(
-//           "Leg " +
-//             i +
-//             " (secs) = " +
-//             stepDuration +
-//             ", accumulated (secs) = " +
-//             prevAccumDuration
-//         );
-//       }
-//       // var marker = new google.maps.Marker({
-//       //   position: {lat: midpointLat, lng: midpointLng},
-//       //   map: map,
-//       //   title: 'Rendezvous Area'
-//       // });
-//       if (status === "OK") {
-//         service = new google.maps.places.PlacesService(map);
-//         midpoint = new google.maps.LatLng(midpointLat, midpointLng);
-//         var request = {
-//           location: midpoint,
-//           radius: "500",
-//           type: ["restaurant"]
-//         };
-//         service.textSearch(request, callback);
-//         let places = [];
-//         function callback(results, status) {
-//           if (status == google.maps.places.PlacesServiceStatus.OK) {
-//             for (var i = 0; i < results.length; i++) {
-//               var place = results[i];
-//               // console.log('map:', map)
-//               // createMarker(results[i]);
-
-//               places.push({
-//                 name: place.name,
-//                 address: place.formatted_address
-//               });
-//             }
-//             console.log("places = ", places);
-
-//             document.getElementById("results").innerHTML =
-//               "<code>" + JSON.stringify(places) + "</code>";
-//           }
-//         }
-//         me.directionsDisplay.setDirections(response);
-//         console.log("me:   ", me);
-//       } else {
-//         window.alert("Directions request failed due to " + status);
-//       }
-//     }
-//   );
-// };
+export default connect(mapState, mapDispatch)(
+  GoogleApiWrapper({
+    apiKey: "AIzaSyAopJDwUG1vlrsZg94qP6yuPtzapUgYw8g"
+  })(MapContainer)
+);
