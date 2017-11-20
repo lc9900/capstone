e297680a83b419d489b40884e7c9b3f63ca3112a
lc9900 <whynot_hm@hotmail.com>
2017-11-20 17:22:38 -0500

    move place_ids to meetup_user_status

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -3,7 +3,6 @@ const Sequelize = db.Sequelize;
 const User = require('./User');
 const Meetup = require('./Meetup');
 const Place = require('./Place');
-const MeetupUserPref = require('./MeetupUserPref');
 const MeetupUserStatus = require('./MeetupUserStatus');
 
 // Association
@@ -20,18 +19,6 @@ Place.hasMany(Meetup);
 // Meetup and User
 Meetup.belongsToMany(User, {through: 'user_meetup_map'});
 User.belongsToMany(Meetup, {through: 'user_meetup_map'});
-
-// Meetup status per user.
-MeetupUserStatus.belongsTo(User);
-MeetupUserStatus.belongsTo(Meetup);
-User.hasMany(MeetupUserStatus);
-Meetup.hasMany(MeetupUserStatus);
-
-//User's preference model
-MeetupUserPref.belongsTo(User);
-MeetupUserPref.belongsTo(Meetup);
-User.hasMany(MeetupUserPref);
-Meetup.hasMany(MeetupUserPref);
 
 // Wrapper
 
