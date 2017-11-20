b81e538d25406e3e02760bc8e9fb28837c8e87d0
lc9900 <whynot_hm@hotmail.com>
2017-11-20 18:48:51 -0500

    remove the useless model

--- server/db/models/MeetupUserPref.js
+++
@@ -1,9 +0,0 @@
-const db = require('../');
-const { Sequelize } = db;
-
-// This table will contain user's preference, like place picks, budget, restaurant type, etc
-const MeetupUserPref = db.define('meetup_user_pref', {
-    place_ids: Sequelize.ARRAY(Sequelize.INTEGER)
-})
-
-module.exports = MeetupUserPref;
