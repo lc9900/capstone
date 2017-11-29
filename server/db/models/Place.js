908e3393499835c59723d44116291dd8029c08fb
AJ Frank <alexanderjfrank@gmail.com>
2017-11-29 16:18:09 -0500

    put route for places

--- server/db/models/Place.js
+++ server/db/models/Place.js
@@ -1,11 +1,13 @@
-const db = require('../');
+const db = require("../");
 const { Sequelize } = db;
 
-const Place = db.define('place', {
-    name: Sequelize.STRING,
-    address: Sequelize.STRING,
-    googleId: Sequelize.STRING,
-    yelpId: Sequelize.INTEGER
-})
+const Place = db.define("place", {
+  name: Sequelize.STRING,
+  address: Sequelize.STRING,
+  googleId: Sequelize.STRING,
+  yelpId: Sequelize.INTEGER,
+  lat: Sequelize.FLOAT,
+  lng: Sequelize.FLOAT
+});
 
 module.exports = Place;
